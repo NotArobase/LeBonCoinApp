f28037c2b1b7774707d26d86be62578580abea9b
LorieFice <thib.magnin@orange.fr>
2024-02-20 18:05:45 +0100

    Gallerie + start photo

--- app/src/main/java/com/example/leboncoinapp/AdAddActivity.java
+++ app/src/main/java/com/example/leboncoinapp/AdAddActivity.java
@@ -1,18 +1,46 @@
 package com.example.leboncoinapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import android.content.ActivityNotFoundException;
+import androidx.core.content.ContextCompat;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
 
-import android.os.Bundle;
+
+
+
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 import com.google.android.material.textfield.TextInputLayout;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
+
 import com.google.android.material.textfield.TextInputEditText;
 
-
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AdAddActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_FROM_GALLERY = 2;
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
+
+
+    private String currentPhotoPath;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,24 +48,136 @@ public class AdAddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ad_add);
 
         TextInputLayout textInputLayout = findViewById(R.id.textInput_titre);
-        TextInputEditText textInputEditText = (TextInputEditText) textInputLayout.getEditText();
-        String titre = textInputEditText != null ? textInputEditText.getText().toString() : "";
-
         TextInputLayout textInputLayoutAdresse = findViewById(R.id.textInput_adresse);
-        TextInputEditText textInputEditTextAdresse = (TextInputEditText) textInputLayoutAdresse.getEditText();
-        String adresse = textInputEditTextAdresse != null ? textInputEditTextAdresse.getText().toString() : "";
+
+        EditText textInputEditText = textInputLayout.getEditText(); // Change to EditText
+        EditText textInputEditTextAdresse = textInputLayoutAdresse.getEditText(); // Change to EditText
 
         Button validateButton = findViewById(R.id.button);
+        Button photoButton = findViewById(R.id.photo);
+        Button galleryButton = findViewById(R.id.gallerie);
+        imageView = findViewById(R.id.imageView1);
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(AdAddActivity.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(AdAddActivity.this, new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+                } else {
+                    // Permission has already been granted, launch camera app
+                    dispatchTakePictureIntent();
+                }
+            }
+        });
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
         validateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String titre = textInputEditText.getText().toString();
                 String adresse = textInputEditTextAdresse.getText().toString();
+
+                if (titre.isEmpty() || adresse.isEmpty()) {
+                    Toast.makeText(AdAddActivity.this, "Il manque des informations", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 Intent intent = new Intent(AdAddActivity.this, AdListViewActivity.class);
                 intent.putExtra("title", titre);
                 intent.putExtra("address", adresse);
+                intent.putExtra("image_path", currentPhotoPath);
                 startActivity(intent);
             }
         });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } catch (ActivityNotFoundException e) {
+                // Camera app is not available or cannot be started
+                Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            } catch (Exception e) {
+                Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+
+        } else {
+            // No app can handle the intent
+            Toast.makeText(this, "No camera app available", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, REQUEST_IMAGE_FROM_GALLERY);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // Do something with the image (e.g., display it in an ImageView)
+        } else if (requestCode == REQUEST_IMAGE_FROM_GALLERY && resultCode == RESULT_OK) {
+            // Image selected from gallery, set it to the ImageView
+            Uri selectedImageUri = data.getData();
+            imageView.setImageURI(selectedImageUri);
+            currentPhotoPath = selectedImageUri.getPath();
+        }
+    }
+
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        imageView.setImageBitmap(bitmap);
     }
 }
